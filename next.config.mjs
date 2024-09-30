737a5ac5672d6cb0166a9023ead130f41f4e4882
Hazrat Ali <hazratali3711@gmail.com>
2024-09-30 10:26:16 +0600

    fixing deployment error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+//   },
+// };
 const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://ewn-api.vercel.app/:path*", // Proxy to Backend
+      },
+    ];
   },
 };
 
